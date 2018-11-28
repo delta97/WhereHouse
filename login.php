e8ef194dc913c3fd190e49ef3be8e84f44f3b3db
Cole Parsons <parson13@purdue.edu>
2018-11-28 13:59:43 -0500

    Update login.php

--- login.php
+++ login.php
@@ -1,28 +1,28 @@
 <?php 
+header('Content-Type: application/json');
 
-$email = $_POST['email'];
-$password = $_POST['password'];
+$post_data = json_decode(file_get_contents('php://input'), true);
+$email = $post_data['email'];
+$password = $post_data['password'];
+
 
 if(session_status() == PHP_SESSION_NONE) {
 	session_start();
 }
 
-$_SESSION['variable_name'] = 1;
 
 require "serverconnect.php";
 $connection = serverConnect();
 
-$email_s = mysqli_real_escape_string($connection, $email);
-$password_s = mysqli_real_escape_string($connection, $password);
 
-$query = "SELECT id, first_name, last_name, user_type FROM User WHERE email = '$email';";
+$query = "SELECT User_id, first_name, last_name, user_type, password FROM User WHERE email = '".$email."';";
 $result = mysqli_query($connection, $query);
 
 if($result) {
-	$assoc_array = mysqli_fetch_assoc($result);
+	$assoc_array = mysqli_fetch_all($result, MYSQLI_ASSOC);
 
 	if($assoc_array["password"] == $password){
-		$user_id = $assoc_array["id"];
+		$user_id = $assoc_array["User_id"];
 		$user_type = $assoc_array["user_type"];
 		$user_first_name = $assoc_array["first_name"];
 		$user_last_name = $assoc_array["last_name"];
