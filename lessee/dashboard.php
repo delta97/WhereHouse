32649d17f648723e8ea1da829c3ed17b731389ae
parke260 <44512260+parke260@users.noreply.github.com>
2018-12-01 18:24:08 -0500

    Update dashboard.php
    changed style

--- lessee/dashboard.php
+++ lessee/dashboard.php
@@ -33,8 +33,62 @@
 		<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
 
 		<!-- Link to the style sheet -->
-		<link rel="stylesheet" href="../style.css"> 
+		<link rel="stylesheet" href="../style.css"> 		
+		
+				<style>
+		body{
+  margin: 0;
+  padding: 0;
+  font-family: 'Roboto Condensed', sans-serif;
+}
+.userbox{
+  width: calc(100%);
+  background: #ffffff;
+  text-align: top;
+  float:top;
+  padding: 10px;
+  border-bottom: 4px solid #e75d20;
+}
+.userbox2{
+  width: calc(100%);
+  background: #ffffff;
+  text-align: top;
+  float:top;
+  padding: 10px;
+  border-bottom: 20px solid white;
+    background: #f2f2f2
 
+}
+.userbox h1{
+  font-family:righteous;
+  font-size: 50px;
+  min-width: 700px;
+}
+.service2{
+  display: inline-block;
+  width: calc(100% / 3);
+  margin: 0 -2px;
+  padding: 25px;
+  box-sizing: border-box;
+}
+.attribute{
+  display: inline-block;
+  width: calc(100% / 2);
+  padding: 55px;
+    margin: 0 -2px;
+
+  box-sizing: border-box;
+}
+
+
+		</style>
+		
+		
+		
+		
+		
+		
+		
 		
 	</head>
 	<body>
@@ -73,20 +127,48 @@
 						<span class="sidebar-btn-text">Account Information</span>
 					</div>
 				</div>
-				<div class="page-content flexbox-dashboard">
-					<div id="user-information" class="dashboard-tile">
-						<div class="dashboard-tile-header">User Information</div>		
+				<div class="page-content">
+			
+			
+		<div class="userbox">
+			<div class="service2">
+			<img src='https://avataaars.io/?avatarStyle=Circle&topType=ShortHairShortWaved&accessoriesType=Wayfarers&hairColor=BrownDark&facialHairType=BeardLight&facialHairColor=Blonde&clotheType=Hoodie&clotheColor=Gray01&eyeType=Close&eyebrowType=UnibrowNatural&mouthType=Smile&skinColor=Yellow'
+			style="width:100pt"/>			
+			</div>
+
+			<div class="service2">
+			<!-- Replace with the user's actual name -->
+			<h1><b>John Doe<b></h1>
+			
+			<!-- Replace with the user's actual email -->
+			<h5>john.doe@purdue.edu</h5>	
+			</div>
+	
+			<div class="service2">
+			<!-- Replace with either their join date, their last contract exp, etc... -->
+			<h5><b>Today's Date:</b> <?php echo date("m/d/Y") ?></h5>
+			<h5><b>Current Time:</b> <?php echo date("h:i"); ?>
+			</div>
+		</div>		
+		
+		
+		
+		
+				<div class="userbox2">
+				<div class="attribute">
+				<div class="dashboard-tile-header">User Information</div>		
 						<div class="dashboard-tile-content">
 							<div id="user-name"><b>Name:</b><span id="user-information-first-name"></span>&nbsp;<span id="user-information-last-name"></span></div>
 							<div id="user-email"><b>Email:</b><span id="user-information-email"></span></a></div>
 							<div id="billing-address"><b>Billing Address:</b> <span id="user-information-address-1"></span>&nbsp;<span id="user-information-address-2"></span>&nbsp;<span id="user-information-city"></span>,&nbsp;<span id="user-information-state"></span>&nbsp;<span id="user-information-zipcode"></span><br></div>
 							<div class="dashboard-btn-flex">
 								<button class="btn btn-primary dashboard-tile-edit" id="edit-information">Edit</button>
-							</div>
-						</div>
-					</div>
-					<div id="user-rentals" class="dashboard-tile dashboard-tile-clickable">
-						<div class="dashboard-tile-header">Your Rentals</div>
+							</div>			
+				</div>
+				</div>
+		
+				<div class="attribute">
+										<div class="dashboard-tile-header">Your Rentals</div>
 						<div class="dashboard-tile-content">
 							<div id="warehouse-1" class="rented-warehouse">
 								<span><b>Warehouse Name</b></span> <br>
@@ -94,20 +176,33 @@
 								&nbsp; &nbsp;<span id="contract-to-1"><b>To: </b> September 14</span>
 							</div>
 						</div>
-					</div>
-					<div id="personalized-search" class="dashboard-tile dashboard-tile-clickable">
+				</div>
+				</div>
+				
+				
+				
+				<div class="userbox2">
+				<div class="attribute">
 						<div class="dashboard-tile-header">Warehouse Search Tailored For You!</div>
 						<div class="dashboard-tile-content">
 							<div class="personalized-search-results">Result 1</div>
 						</div>
-					</div>
-					<div id="current-requests" class="dashboard-tile dashboard-tile-clickable">
-						<div class="dashboard-tile-header">Pending Requests</div>
+				</div>
+		
+				<div class="attribute">
+										<div class="dashboard-tile-header">Pending Requests</div>
 						<div class="dashboard-tile-content">
 							<div id="request-1"><b>Warehouse Name</b></div>
 							<div id="request-1-date">August 16, 2018</div>
 							<div id="request-time">8:52 PM</div>
 						</div>
+				</div>
+				</div>
+				
+				
+		
+					<div id="current-requests" class="dashboard-tile dashboard-tile-clickable">
+						
 					</div>
 				</div>
 			</div>
@@ -172,7 +267,6 @@
 		$('#edit-information').on('click', function(event){
 			window.location = "account_info.php";
 		});
-
 		//search field actions
 		$('#zip-search').on('focusout', function(event) {
 			var searchQuery = $('#zip-search').val();
@@ -187,12 +281,7 @@
 		$("#user-rentals").on("click", function(event) {
 			window.location = "rentals.php";
 		});
-
-
-
-
 		$(document).ready(function(event){
-
 			$('.header-username').text("Welcome, "+ sessionStorage.getItem("user_first_name"));
 			//populate the user infromation tile on the dashboard 
 			$.ajax({
@@ -209,7 +298,6 @@
 					var city = response['city'];
 					var state = response['state'];
 					var zipcode = response['zipcode'];
-
 					if(address_2 = null) {
 						address_2 = "";
 					}
@@ -221,28 +309,19 @@
 					$('#user-information-city').text(city);
 					$('#user-information-state').text(state);
 					$('#user-information-email').text(zipcode);
-
 					//setting session items for values not initialized in the login
 					sessionStorage.setItem("user_address_1", address_1);
 					sessionStorage.setItem("user_address_2", address_2);
 					sessionStorage.setItem("user_city", city);
 					sessionStorage.setItem("user_state", state);
 					sessionStorage.setItem("user_zipcode", zipcode);
-
-
 				}
 			});	
 			//populate the sidebar with notification icons as needed
 			get_notification_badges();
-
 	});
-
-
-
-
 		//function that populates the sidebar with notification icons
 		function get_notification_badges() {
-
 			$.ajax({
 				url: 'get_notification_badges.php',
 				type:'post',
@@ -256,7 +335,6 @@
 					$('.notification-span-rentals').text(num_rentals);
 					$('.notification-span-requests').text(num_requests);
 					$('.notification-span-messages').text(num_unchecked_messages);
-
 				}
 			});
 		}
@@ -265,6 +343,5 @@
 				console.log(response);
 			});
 		}
-
 	</script>
 </html>
