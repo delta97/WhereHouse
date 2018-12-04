9542f20c6c26096cacefb20dce7af57432becf26
Amanda Crowe <44512142+crowe13@users.noreply.github.com>
2018-12-04 11:33:37 -0500

    Update analytics.R
    changed to facet_grid from facet_wrap

--- R/analytics.R
+++ R/analytics.R
@@ -21,18 +21,18 @@ a$End_month <- month(convert2)
 
 df <- a %>% group_by(Owner_ID,WH_ID,Start_month) %>% dplyr::summarise(sum(TotPrice)) 
 #Will need to set Owner_ID equal to session variable here
-df1 <- df[which(df$Owner_ID == 288),]
+df1 <- df[which(df$Owner_ID == 13457),]
 warehouses <- df1 %>%  distinct(WH_ID)
 
-#Update to SQL varaibles
+#Update to SQL variables
 ggplot(df1, aes(x = df1$Start_month, y = df1$`sum(TotPrice)`)) +
   geom_point() + 
   geom_line()+
-  facet_wrap(~df1$WH_ID, scales='free', labeller = label_parsed) +
+  #Change the df1$WH_ID below to address of warehouse somehow so that it's displayed on the graphs but it needs to be in the df1 data frame
+  facet_grid(df1$WH_ID~., scales='free_x', labeller = label_parsed) +
   labs(title="Monthly Revenue Time Series Chart for 2018 Contracts", 
        subtitle="Plots seperated by your Warehouse's IDs", 
        y="Returns $",x="Months") +
   scale_x_continuous(breaks = round(seq(min(df1$Start_month), max(df1$Start_month), by = 1),1)) +
   theme_gray() + scale_colour_economist() 
-
 
