55c17c2981a91493f7fb86f268a792fa8ff6a3b3
campb287 <44512086+campb287@users.noreply.github.com>
2018-12-05 02:10:23 -0500

    Update hashing.R

--- R/hashing.R
+++ R/hashing.R
@@ -1,3 +1,20 @@
+#Hashes Existing Passwords
+get_passwords <- function(){
+  
+  mydb <- dbConnect(MySQL(), user='g1090429', password='WhereHouse?', dbname='g1090429', host='mydb.ics.purdue.edu')
+  
+  rs <- dbSendQuery(mydb, "SELECT password FROM User;")
+  data <- fetch(rs, n=-1)
+  
+  new_pass <- apply(cyka, 1, gen_hash)
+  
+  dbDisconnect(mydb)
+  
+  return(new_pass)
+  
+}
+
+
 #Generates Random String
 test_hash <- function(input, stored){
   
